d9bde60cfdb4116ee6e20f17cef5f4c3dcf3cfe1
ajay <infantcsajay@outlook.com>
2023-05-06 05:41:26 +0530

    initial commit

---
+++ lib/expenses_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Expenses App');
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:expenses_app/expenses_app.dart';
+
+void main() {
+  runApp(const ExpensesApp());
+}
